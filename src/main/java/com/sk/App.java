a6fa21e8bc0b7a78a9221fddab50b31d97252833
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2022-06-14 06:59:48 +0900

    hello controller 및 롬복 테스트

--- src/main/java/com/sk/App.java
+++ src/main/java/com/sk/App.java
@@ -1,12 +1,39 @@
 package com.sk;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @RestController
+    class HelloController {
+
+        @GetMapping("/hello")
+        public String hello() {
+            return "hello";
+        }
+
+        @GetMapping("/hello/dto")
+        public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                         @RequestParam("amount") int amount){
+            return new HelloResponseDto(name, amount);
+        }
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    static class HelloResponseDto{
+        private final String name;
+        private final int amount;
     }
 
 }
